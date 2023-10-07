0a1ba19fb24ee5765db118bc4bc7284cfd3bf987
none <none>
2023-10-08 00:31:43 +0300

    Addded adaptivity, new design

--- homepage.js
+++ homepage.js
@@ -1,5 +1,5 @@
 const reset = document.querySelector(".reset");
-const output = document.querySelector(".turn");
+const output = document.querySelector(".output");
 const btns = document.querySelectorAll(".b");
 
 let count = 0;
@@ -12,26 +12,28 @@ let board = [  '', '', '',
 function won() {
   for (let i = 0; i < board.length; i += 3) {
     if (board[i] !== '' && board[i] === board[i+1] && board[i+1] === board[i+2]) {
-		output.innerHTML = `Player ${board[i]} won!`;
+		output.innerHTML = `Player <span class="won">${board[i]}</span> won!`;
 		isFinished = true;
     }
   }
   for (let j = 0; j < 3; j++) {
     if (board[j] !== '' && board[j] === board[j+3] && board[j+3] === board[j+6]) {
-		output.innerHTML = `Player ${board[j]} won!`;
+		output.innerHTML = `Player <span class="won">${board[j]}</span> won!`;
 		isFinished = true;
     }
   }
     if (board[0] !== '' && board[0] === board[4] && board[4] === board[8]) {
-    output.innerHTML = `Player ${board[0]} won!`;
+    output.innerHTML = `Player <span class="won">${board[0]}</span> won!`;
 	isFinished = true;
   }
     if (board[2] !== '' && board[2] === board[4] && board[4] === board[6]) {
-    output.innerHTML = `Player ${board[2]} won!`;
+    output.innerHTML = `Player <span class="won">${board[2]}</span> won!`;
 	isFinished = true;
   }
   	if (!board.includes('')){
-		output.innerHTML = 'Draw!'
+		if(!isFinished){
+			output.innerHTML = 'Draw!'
+		}
 	}
 }
 
@@ -49,14 +51,14 @@ for(let i = 0; i < btns.length ; i++){
 		count++;
 
 		 if (count % 2 === 0){
-			 btns[i].innerHTML = "O";
-			  output.innerHTML = "Now player X!";
+			 btns[i].innerHTML = '<span class ="O"> O </span>';
+			  output.innerHTML = 'Now player <span class ="X"> X </span>!';
 				 btns[i].disabled = true;
 				  board[i] = "O";
 				   finish()
 		 } else {
-			 btns[i].innerHTML = "X"
-		  	  output.innerHTML = "Now player O!"
+			 btns[i].innerHTML = '<span class ="X"> X </span>'
+		  	  output.innerHTML = 'Now player <span class ="O"> O </span>!'
 			    btns[i].disabled = true;
 				 board[i] = "X";
 				  finish()
